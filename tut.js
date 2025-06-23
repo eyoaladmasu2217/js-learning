f88de0767ccae7b52a046b005322e59b33801f16
Eyoal Yeshewas Admasu <eyoaladmasu@gmail.com>
2025-06-23 03:09:29 -0700

    Initial commit

---
+++ tut.js
@@ -0,0 +1,18 @@
+// const name2 = ["eyoal","yeshewas"];
+// const name3 = [...name2, "admasu"];
+// console.log(name3);
+
+// let name = ["eyoal", "yeshewas", "admasu"];
+
+// let result =name.map((name) => {
+//    return name + "1";
+// });
+
+
+// console.log(result);
+let name =["Bisrat", "Eyoal", "Yeshewas", "Admasu"];
+
+let result2 = name.filter((name) =>{
+    return name.length > 5;
+});
+console.log(result2);
